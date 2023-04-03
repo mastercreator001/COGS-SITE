37b6f522b856315ca27339cac30f8f1a129b590a
mastercreator001 <melon_man2000@hotmail.com>
2023-04-03 22:42:00 +0100

    03/04/23/2

--- src/layouts/sections/navigation/nav-tabs/Schoolday.vue
+++ src/layouts/sections/navigation/nav-tabs/Schoolday.vue
@@ -27,38 +27,71 @@ onMounted(() => {
     text="Start time"
     :breadcrumb="[{ label: 'Navigation', route: 'school' }, { label: 'School day' }]"
   >
-  <img src="src/assets/img/schoolhistorictotaloverview1.png" title="School day itinerary">
-  <img src= "assets/img/schoolhistorictotaloverview1.png">
+  
+  <img src="/src/assets/img/schoolDay/schoolDiary.png" alt="My school diary ">
     <View title="School Day structure"   
-    subtitle="The structure of the school day "
-    :code="TabsSimpleCode" id="tabs-simple">
-      
+    subtitle="The structure of the school day all started with your time table, do you still have yours? "
+    >
+      </View>
+    <img src="/src/assets/img/schoolDay/09.10Clock.png" alt="09.10">
+    <View title="School Start time 
+    1) 09:10  was registration, in form class">
+      <inputOutlined />
+    </View>
     
-    <TabsSimple />
-    </View>
-
-    <View title="School Start time 0910 or was it 0900">
+    <img src="/src/assets/img/schoolDay/09.15Clock.png" alt="09.15">
+    <View title="School 1st lesson period
+    2) 09:15 to 10.25 1st lesson dependant on your diary and the day dictated your class or form group.">
       <inputOutlined />
     </View>
 
-    <View title="Early Morning break 10 am for 10 minutes or 20 minutes">
+    <img src="/src/assets/img/schoolDay/10.25Clock.png" alt="10.25">
+    <View title="School 2nd lesson period
+    3) 10.25 to 11.35  2nd lesson dependant on your diary and the day dictated your class or form group.">
       <inputOutlined />
     </View>
 
-    <View title="Lunchtime 12:30 or 1pm" :code="inputIconCode" id="input-with-icon">
-      <InputIcon />
+    <img src="/src/assets/img/schoolDay/11.35Clock.png" alt="11.35">
+    <View title="School morning break time 20 minutes
+    4) 11.35 to 11.55 and break time had arrived, for 20 minutes.">
+      <inputOutlined />
     </View>
 
-    <View title="Was the lunch break split up to allow for space in the dinning hall?" :code="inputSuccessCode" id="input-success">
-      <InputSuccess />
+    <img src="/src/assets/img/schoolDay/11.55Clock.png" alt="11.55">
+    <View title="School 3rd lesson period
+    5) 11.55 to 13.05 3rd lesson dependant on your diary and the day dictated your class or form group.">
+      <inputOutlined />
     </View>
 
-    <View title="No Afternoon break but do you remember the school finish time? 15:40 seems correct to me" :code="inputErrorCode" id="input-error">
-      <InputError />
+    <img src="/src/assets/img/schoolDay/14.05Clock.png" alt="14.05">
+    <View title="School 4th lesson period
+    6) 14.05 to 1500 4th lesson dependant on your diary and the day dictated your class or form group.">
+      <inputOutlined />
     </View>
 
-    <View title="How was the lunchtime started? and the home time given, not that we need any asking to head home" :code="inputDisabledCode" id="input-disabled">
-      <InputDisabled />
+    <img src="/src/assets/img/schoolDay/hometime.png" alt="home time">
+    <View title="Home time and awaiting the bell.
+    4) 15.00 was home time, bus walk car? whichever you did it was a relief.">
+      <inputOutlined />
+    </View>
+
+    <img src="/src/assets/img/schoolDay/assemblyTime.png" alt="Assembly">
+    <View title="School assemblies ? once a week or more ">
+      <inputOutlined />
+    </View>
+
+    <View title="Lunchtime 12:30 or 1pm or did you have a different time ?">
+    </View>
+
+    <View title="Was the lunch break split up to allow for space in the dinning hall?" >
+      
+    </View>
+
+    <View title="No Afternoon break but do you remember the school finish time? 15:40 seems correct to me or perhaps as per my school diary it was 15.00 or shortly after." >
+    </View>
+
+    <img src="/src/assets/img/schoolhistorictotaloverview1.png" title="School day itinerary">
+    <View title="How was the lunchtime started? and the home time given, not that we need any asking to head home">
     </View>
   </BaseLayout>
 </template>
--- src/layouts/sections/navigation/navbars/OpeningOfTheSchool.vue
+++ src/layouts/sections/navigation/navbars/OpeningOfTheSchool.vue
@@ -34,28 +34,25 @@ onMounted(() => {
     </View>
 
     <img src="/src/assets/img/school maps/advancedMap.jpg" title=" an early iamge of the school post construction">
-    <View title="The School as any would have been well planned for and as such plans were generated to faciltate a well administered school built within approvals from the local council. As you can see above with the map recreated from an original the basic layout of the school was decided years previously. ">
+    <View title="The School as any would have been well planned for and as such plans were generated to facilitate a well built school within approvals from the local council and conforming to the general needs of it's intended future use. As you can see above with the map recreated from an original the basic layout of the school was decided years previously by a local arcitect in consultation with the Diosease Of Portsmouth and the local council. ">
       <inputOutlined />
     </View>
 
     <img src="/src/assets/img/school maps/bookMap.jpg" title="A map from the opening informatio leaflet given to staff and visitors upon opening">
-  
-    <View title="Decisions Decisions the school was ahead of it's time and was as you'd expect fairly innovative in regards to it's technology and design boasting A new computer room and study areas and a full library or as they are now termed a resource center.    " :code="inputIconCode" id="input-with-icon">
-      <InputIcon />
+    <View title="Decisions Decisions the school was ahead of it's time and was as you'd expect fairly innovative in regards to it's technology and design boasting A new computer room and study areas and a full library or as they are now termed a resource center. ">
     </View>
 
-    <img src="src/assets/imgschool maps/schoolOpeningPamphlet.png" title="school opening pamphlet">
-    <View title="Many changes undoubtely were decided before the school inception but some such as landscaping were although mostly preplanned were finally decided rather later. All part of the design procees but the school was to be a natural wondrland as well as a school. Indeed it its later days it actually had a nature reserve and a pond to compliment all the extra trees and predetermined landscaping already decided upon in the planning phase. " :code="inputSuccessCode" id="input-success">
-      <InputSuccess />
+    <img src="/src/assets/img/school maps/schoolOpeningPamphlet.png" title="school opening pamphlet">
+    <View title="Many changes undoubtely were decided before the school inception but some such as landscaping were although mostly preplanned were finally decided rather later. All part of the design procees but the school was to be a natural wondrland as well as a school. Indeed it its later days it actually had a nature reserve and a pond to compliment all the extra trees and predetermined landscaping already decided upon in the planning phase. " >
     </View>
 
     <img src="/src/assets/img/school maps/finalMapCTK.png" title="A final map of the school in it's pre demolition phase">
-    <View title="A rather local affair, although the school had influence from the Diosease of Portsmouth and indeed The Church Of England it was designed by local architects and the materials where pratical were also sourced locally. The builders then would naturally be local as well as was the case in this construction effort. Although the school was new it had the innovations to stand above the rest with a new modern design and all the sports and gym equiptment to match any of the leading schools  " :code="inputErrorCode" id="input-error">
-      <InputError />
+    <View title="A rather local affair, although the school had influence from the Diosease of Portsmouth and indeed The Church Of England it was designed by local architects and the materials where pratical were also sourced locally. The builders then would naturally be local as well as was the case in this construction effort. Although the school was new it had the innovations to stand above the rest with a new modern design and all the sports and gym equiptment to match any of the leading schools.">
     </View>
 
-    <View title="Funding was a joint venture for the school with assets being relaised and matched between IWCC and the Doisease Of Portsmouth as the main fund contributers. There was also a round of funding to keep the school ahead of the rest as parents were also asked to add into the schools funds for general repair maintenance and upkeep anoung other things. Primarily by the time parents were asked to assist the school had been finished and was in general use. " :code="inputDisabledCode" id="input-disabled">
-      <InputDisabled />
+    <img src="/src/assets/img/school maps/frontCoverPam.png" alt="Front cover of intial release document">
+    <View title="Funding was a joint venture for the school with assets being relaised and matched between IWCC and the Diosease Of Portsmouth as the main fund contributers. There was also a round of funding to keep the school ahead of the rest as parents were also asked to add into the schools funds for general repair maintenance and upkeep anoung other things. Primarily by the time parents were asked to assist the school had been finished and was in general use.">
+      
     </View>
   </BaseLayout>
 </template>
