13dcffe69923a630c009522d78e21a235ff8924f
mastercreator001 <melon_man2000@hotmail.com>
2023-04-16 09:18:24 +0100

    16/04/23/1

--- src/layouts/sections/navigation/navbars/OpeningOfTheSchool.vue
+++ src/layouts/sections/navigation/navbars/OpeningOfTheSchool.vue
@@ -19,12 +19,32 @@ onMounted(() => {
   setNavPills();
 });
 </script>
+
+<script>
+export default {
+  data() {
+    return {
+      ImagePopupTitle: '',
+      ImagePopupSrc: '',
+      ImagePopupShow: false
+    }
+  },
+  methods: {
+    openImage: function(elem) {
+      this.ImagePopupShow = false
+      this.ImagePopupTitle = elem.srcElement.alt
+      this.ImagePopupSrc = elem.srcElement.src
+      this.ImagePopupShow = true
+    }
+  }
+}
+</script>
 <template>
   <BaseLayout
     title="Opening of the School early map"
     :breadcrumb="[{ label: 'Navigation', route: '#' }]"
   >
-  <img src="@/assets/img/school maps/SCHOOLPLANFINAL.png" title="A reworked Image of the school in it's design phase">
+  <img src="@/assets/img/school maps/SCHOOLPLANFINAL.png" title="A reworked Image of the school in it's design phase" @click="openImage">
     <View
       title="A reworked Image of the school in it's design phase, built from an original image in Inkscape and created into a svg image"
       id="School map 1"
@@ -33,26 +53,26 @@ onMounted(() => {
       <schoolmap  />
     </View>
 
-    <img src="@/assets/img/school maps/advancedMap.jpg" title=" an early iamge of the school post construction">
+    <img src="@/assets/img/school maps/advancedMap.jpg" title=" an early iamge of the school post construction" @click="openImage">
     <View title="The school was disolved on 1 September 2008 when two middle schools on Wellington Road, Archbishop King Catholic Middle and Trinity Church of England Middle merged making the newly Formed Christ The King College. This was done in line with the education reforms that were being implemented to schools across the island at the time.The desire to expand beyond the then-current age range of 9–13 years was first registered in December 2004, shortly after a new headteacher, Pat Goodhead, was appointed. In 2006, after outstanding Ofsted inspections for Trinity, and the need for a new headteacher for Archbishop King, a joint governing body committee was formed and Pat Goodhead was appointed headteacher of both Trinity and Archbishop King. The amalgamation of the two schools moved on as there were positive responses from all involved, including clergy, parents and staff, which was confirmed during an official review. The schools were fully amalgamated in September 2008, resulting in the closure of Trinity and Archbishop King middle schools.
     
     The School as any would have been well planned for and as such plans were generated to facilitate a well built school within approvals from the local council and conforming to the general needs of it's intended future use. As you can see above with the map recreated from an original the basic layout of the school was decided years previously by a local arcitect in consultation with the Diosease Of Portsmouth and the local council. ">
       <inputOutlined />
     </View>
 
-    <img src="@/assets/img/school maps/bookMap.jpg" title="A map from the opening informatio leaflet given to staff and visitors upon opening">
+    <img src="@/assets/img/school maps/bookMap.jpg" title="A map from the opening informatio leaflet given to staff and visitors upon opening" @click="openImage">
     <View title="Decisions Decisions the school was ahead of it's time and was as you'd expect fairly innovative in regards to it's technology and design boasting A new computer room and study areas and a full library or as they are now termed a resource center. ">
     </View>
 
-    <img src="@/assets/img/school maps/schoolOpeningPamphlet.png" title="school opening pamphlet">
+    <img src="@/assets/img/school maps/schoolOpeningPamphlet.png" title="school opening pamphlet" @click="openImage">
     <View title="Many changes undoubtely were decided before the school inception but some such as landscaping were although mostly preplanned were finally decided rather later. All part of the design procees but the school was to be a natural wondrland as well as a school. Indeed it its later days it actually had a nature reserve and a pond to compliment all the extra trees and predetermined landscaping already decided upon in the planning phase. " >
     </View>
 
-    <img src="@/assets/img/school maps/finalMapCTK.png" title="A final map of the school in it's pre demolition phase">
+    <img src="@/assets/img/school maps/finalMapCTK.png" title="A final map of the school in it's pre demolition phase" @click="openImage">
     <View title="A rather local affair, although the school had influence from the Diosease of Portsmouth and indeed The Church Of England it was designed by local architects and the materials where pratical were also sourced locally. The builders then would naturally be local as well as was the case in this construction effort. Although the school was new it had the innovations to stand above the rest with a new modern design and all the sports and gym equiptment to match any of the leading schools.">
     </View>
 
-    <img src="@/assets/img/school maps/frontCoverPam.png" alt="Front cover of intial release document">
+    <img src="@/assets/img/school maps/frontCoverPam.png"  alt="Front cover of intial release document" @click="openImage">
     <View title="Funding was a joint venture for the school with assets being relaised and matched between IWCC and the Diosease Of Portsmouth as the main fund contributers. There was also a round of funding to keep the school ahead of the rest as parents were also asked to add into the schools funds for general repair maintenance and upkeep anoung other things. Primarily by the time parents were asked to assist the school had been finished and was in general use.">
       
     </View>
--- src/layouts/sections/page-sections/features/SchoolTeachers.vue
+++ src/layouts/sections/page-sections/features/SchoolTeachers.vue
@@ -48,7 +48,7 @@ export default {
     ]"
     
   >
-  <img src="/src/assets/img/school/school staff/teacherFeature/teachersGroupMalham.png" title="Senoir Staff">
+  <img src="/src/assets/img/school/school staff/teacherFeature/teachersGroupMalham.png" title="Senoir Staff" @click="openImage">
     <View title="School teachers at the Malham third year week away outing">
       
     </View>
@@ -59,7 +59,7 @@ export default {
      
     </View>
 
-    <img src="/src/assets/img/school/school staff/teacherFeature/missForer.png"  alt="Miss Forer with student" @click="openImage">
+    <img src="/src/assets/img/school/school staff/teacherFeature/missForer2.png"  alt="Miss Forer with student" @click="openImage">
     <img src="/src/assets/img/school/school staff/teacherFeature/mrWalker.png" alt="" @click="openImage">
     <View title="Miss Forer and Mr Walker"  id="input-success">
       
@@ -73,7 +73,7 @@ export default {
             
     </View>
 
-    <img src="/src/assets/img/school/school staff/teacherFeature/mrsHayles.png" alt="Mrs Hayles maths">
+    <img src="/src/assets/img/school/school staff/teacherFeature/mrsHayles.png" alt="Mrs Hayles maths" @click="openImage">
     <View title="Mrs Hayles, Mrs Besley" >
       
     </View>
@@ -83,7 +83,7 @@ export default {
     If we could have some awesome memories of this special lady it would be great maybe where she us now what she has done since if you know. ">
      </View>
 
-    <img src="/src/assets/img/school/school staff/teacherFeature/mrsPassmore.png" alt="Mrs Passmore art">
+    <img src="/src/assets/img/school/school staff/teacherFeature/mrsPassmore.png" alt="Mrs Passmore art" @click="openImage">
     <View title="Art Mrs Passmore, Mr Patterson Mr O'Donohue"  id="input-disabled"></View>
     <View title="Let's take a trip into the art department and today we are visiting Mrs Passmore. What happened to her I remember her being my art teacher during my 87 through 90 time at the school but she seems to have been there for only a while. It seems she joined as a temporary teacher in 1984 as a replacment for the previous teacher Mr O Donaghue who never returned, thanks Sarah.
     Do you know her or where she went or perhaps your related to her. Or maybe you can add a part of the puzzle were missing here.
@@ -93,8 +93,9 @@ export default {
     I also remember her driving her yellow Mercedes Benz and as I recall a very expensive car in the day so maybe she was a teacher for the live of the job. I'm keen to hear of your thoughts for this memorable teacher and would love to find out if she inspired you to do something artsy with your life." >
     </View>
 
-    <img src="/src/assets/img/school/school staff/teacherFeature/mrTansley2.png" alt="Science staff Mr Tansley">
-    <img src="/src/assets/img/school/school staff/teacherFeature/mrMoorse.png" alt="Science staff Mr Moorse">
+    <img src="/src/assets/img/school/school staff/teacherFeature/mrTansley2.png" alt="Science staff Mr Tansley" 
+    @click="openImage">
+    <img src="/src/assets/img/school/school staff/teacherFeature/mrMoorse.png" alt="Science staff Mr Moorse " @click="openImage">
     <View title="Science Teachers Mr Tansley, Mr Moorse, Mr Monk">
      </View>
      <View title="We are visiting the science department today and a true icon of that department. I remember him as I'm sure many of you will as a cleaver man who was not only good at his job but a fun man to be around as well.
@@ -108,9 +109,9 @@ export default {
     Mr Moorse was a lab technician in the science department helping Mr Tansley with the amazing experiments we were all in awe about and intrducing us to the world of Scientific discovery. Science was fun as it was interesting curious and mysterious all at the same time.Mr Moorse joined the school in the mid to late eighties and had been a relatively young chap at the time. Ambitious and keen so when he joined us in out Malham trip in 1989 you expected fun and he didn't disappoint. Playing football with the boys and being one of the lads he had an unfortunate accident and damaged his leg. Resulting in him being taken to the hospital by a teacher and coming back with his leg either heavily or bandaged or in plaster. Opps being the operative word perhaps some one with a longer memory can help me with this. Mr Moorse left C Of E Middle School to become a deputy head at Osbourne Middle school and eventually transferred Cowes Enterprise College and started teaching Maths, a man of many talents Science Maths and a born leader. He has been very productive over the years as I have read in previous posts leaving C of E to move onto bigger better and greater posts. So if we can build a picture of him joining leaving and a full set of fun memories of this memorable teacher please do.">
      </View>
     
-    <img src="/src/assets/img/school/school staff/teacherFeature/mrsBall.png" alt="Mrs Ball Compilation">
+    <img src="/src/assets/img/school/school staff/teacherFeature/mrsBall.png" alt="Mrs Ball Compilation " @click="openImage">
     
-     <View title="Physicial Education Teachers Mrs Ball, Mr Christensen(Aka Mr C), Mr Hind, MsvSue Rayner, Mr Baggot Mrs Honor "> 
+     <View title="Physicial Education Teachers Mrs Ball, Mr Christensen(Aka Mr C), Mr Hind, Ms Sue Rayner, Mr Baggot, Mrs Honor "> 
       </View>
     <View title="Today we have Mrs Ball or at least as far as memory serves we do. I believe she was a physical education teacher so we're hoping to get some great memories of this well deserved teacher. I'm sure she was at the school in the mid to end of the eighties and certainly into the later years at the school working into the formation of Christ The King College and now forms part of the school history.
     Mrs Ball was popular amoung the students and was famed for her netball and hockey classes also having been married to (ironically I hear you say) Mr Ball who was also a P.E teacher at Carisbrooke High having teaching differing age grops I'm sure they had some intersting conversations.
@@ -126,8 +127,8 @@ export default {
       </View>
 
 
-    <img src="/src/assets/img/school/school staff/teacherFeature/mrsReynolds.png" alt="Mrs Reynolds">
-    <img src="/src/assets/img/school/school staff/teacherFeature/missForer1.png" alt="Miss Forer">
+    <img src="/src/assets/img/school/school staff/teacherFeature/mrsReynolds1.png" alt="Mrs Reynolds " @click="openImage">
+    <img src="/src/assets/img/school/school staff/teacherFeature/missForer2.png" alt="Miss Forer " @click="openImage">
     <View title="Mrs Reynolds+ Miss Forer">
       </View>
     
@@ -138,7 +139,7 @@ export default {
       
    
     <img src="/src/assets/img/school/school staff/teacherFeature/mrFelton.png" alt="Mr Felton school photo">
-    <View title="Mr Felton Design and Technology previously deputy Mr Luckett Mr Darby"  id="Early Pictures">
+    <View title="Mr Felton Design and Technology previously deputy Mr Luckett Mr Darby"  id="Early Pictures " @click="openImage">
      
     </View>
     <View title="We all know him as Mr Felton and he had been at the school serving for decades, and another dedicated teacher in the C of E middle community.
@@ -148,7 +149,7 @@ export default {
      </View>
 
     
-     <img src="/src/assets/img/school/school staff/teacherFeature/mrsMurdoch.png" alt="Mrs Murdoch Domestic Science">
+     <img src="/src/assets/img/school/school staff/teacherFeature/mrsMurdoch.png" alt="Mrs Murdoch Domestic Science " @click="openImage">
     <View title="Mrs Murcoch"  id="Mrs Murdoch, AKA super gran">
       
     </View>
@@ -160,20 +161,60 @@ export default {
     Mrs Murdoch is the pinnacle of teaching for me and I'm definatly a big fan so hardly impartial.">
       </View>
     
-    <img src="/src/assets/img/school/school staff/teacherFeature/mrsBrock.png" alt="Mrs Brock, Miss Goodship, Music teacher">
-    <View title="Music Teachers"  id="input-error">
+    <img src="/src/assets/img/school/school staff/teacherFeature/mrsBrock.png" alt="Mrs Brock, Miss Goodship, Music teacher" @click="openImage">
+    <View title="Music Teachers Mrs Brock, Miss Goodship, any others please let us know"  id="Mrs Brock">
       </View>
     
-    <View title="Music Teachers"  id="input-error">
+    <View title="Welcome back, as they say music is the heart and soul and for this lady it seems to be a literal statement. Yes that right we are visiting Mrs Brock as it has been previously mentioned to me Mrs Brock was a music teacher composer and very talent individual whom music is everything.
+    Speaking with Miss Forer who never fails to impress in herself she cannot sing Mrs Michelle Brock talents loud enough.
+    Having been at the school for a number of years her leadership through the musical department has been nothing short of seismic. I was at the school and she was instrumental in the choreography execution and even writing of pieces used in school plays and many others including The evacuees 1990, To everything a season (Miss Forer' s leaving show 1996) Yanomamo and old Tyme music and ocean world I'm certain I am not doing her justice as many more production would have taken place so if you know of one please do feel free to let me know of the performances Mrs Brock worked on and I will amend the post.
+    I believe Mrs Brock took over from Mrs Goodship but we will revisit the music department again soon. Was Mrs Brock involved in the Old Tyme Music Hall production held at Sandown pavilion ?
+    I'm sure we will find relive many memories in this post were you good at music did you get help learn a knew instrument at all.  
+    Your memories count and a message to members of  C of E Middle School this is a special place for students and teacher alike let's keep it that way as a group for us. 
+    Please only add members who went to the school and can contribute to the group as this is our place and it is special.">
      </View>
 
-  
+    <img src="/src/assets/img/school/school staff/teacherFeature/mrsChalloner.png" alt="Mrs Challoner " @click="openImage">
+    <img src="/src/assets/img/school/school staff/teacherFeature/mrsReynolds1.png" alt="Mrs Reynolds" @click="openImage">
     <View title="Religious Educataion Mrs Challoner and Mrs Reynolds, Miss Howe  "  id="Mrs Challoner and Mrs Reynolds">
-     
-    </View>
+     </View>
+     <View> Hello to Mrs Challoner apart from the lovely smile of course. 
+      I have to confess I don't have allot of information on Mrs Challoner but which subject did she teach was it religious education or French. 
+      She was neither my year group or my tutor group so I have had little interaction please can we see if we can get some work history and some fantastic memories. 
+      My apologise about the photos I can't find any information on school groups and have had to look through the archive for screen shots.  Do you have any great pictures of this lady having fun.
+    
+      Mrs Reynolds was a fanatastic person Sad but true it with sadness I say Mrs Reynolds is no longer with us but I'm sure she is fondly remembered by you guys and girls. 
+      I'm afraid exclusively the fact she was an ace teacher who totally rocked with religious education she was also a form tutor .
+      She has been a friend a teacher a confidant and much much more. My dealings with Mrs Reynolds are limited as I never had her as a tutor or as far as my limited memory goes a teacher that said I feel sure we will discover new things we all forgot and can celebrate a fantastic part of the C of E community at Trinity.</View>
+    
 
-    <View title="History Miss Watkins, Miss Dennis Mr Besley "  id="History ">
+      <img src="/src/assets/img/school/school staff/teacherFeature/mrsWatkins.png" alt="Mrs Watkins/ Cobby/ Mountgomery" @click="openImage">
+      <img src="/src/assets/img/school/school staff/teacherFeature/missDennis.png" alt="Miss Dennis">
+    <View title="History Miss Watkins, Miss Dennis Mr Besley "  id="History " @click="openImage">
+     </View>
+
+   <View title="Hello to Mrs Watkins she remarried into Mrs Cobby and is now Mrs Montgomery 
+      so from here to the end of the post we shall use her new name just so it doesn't get to confusing .
+      Mrs Montgomery joined the school in April 1980 to cover Mr Luckett 's time table so it wasn't a long term thing but fortunately enough we have kept her for longer.  
+      She progressed from there to Mrs Googes classed while she was away on maternity leave. Her time table progressed over the years and she stayed with us making many happy memories and contributing to the school through her teaching talent and participation in many school activities like Malham.
+      Mrs Montgomery was a specialist teacher and taught years 3 and 4 or 7 and 8 dependant on your years served.
+      Teaching English French and history she stayed at the school covering many subjects anc becoming an integral part of the staff. After I imagine was only supposed to be a short term posting to the school.
+      Having been there for so long as was the tradition for teachers it seems at C of E she contributed to the education of many thousand of students over the years and making happy memories along the way. 
+      Remember Mrs Watkins is now Mrs Montgomery so please do feel free to say thank you as she's looking at the post. 
+      Staying in one place if any thing is a sign of commitment love of the job and the students.
+      Linda Montgomery sadly left the school in 2012 after 32 years service but is happily in the group and still a part of the community. "  id="Mrs Mountgomery">
+     </View>
+    
+    <View title="Hello Miss Dennis a teacher who served the school as 
+      many of the other did. 
+      Dedication and good principles must have been  deeper than a student would have noticed as they certainly did retain their staff at C of E middle.
+      Know I don't have allot of information on Miss Dennis so I need your help and I will add your comments or factual information you can present from the comments into the post as we go.
+      Miss Dennis was teaching at C of E middle during my time between 1987 and 1990 but when did she start and leave can you help fill in those blanks for me. 
+      Whag was her specialist subject as these seem to vary over the years as teachers seemed to cover subject for absences and floated between. That especially taken on for a particular gifted one, was if RE or will I be proven wrong ??
+      Please feel free to add your thoughts and any good factual information would be appreciated so we can amend the post and add detail. 
+      Was Miss Dennis a vicar at Parkhurst upon leaving  C of E Middle school of may be you know her and can help with a time line of her following movements. "  id="Miss Denis">
+     </View>
+
      
-    </View>
   </BaseLayout>
 </template>
